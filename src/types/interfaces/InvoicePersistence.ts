6a2610098d948ff38ebeda97cd46e9ad4943a3a4
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-10-20 17:19:19 -0600

    interface having optional property

--- src/types/interfaces/InvoicePersistence.ts
+++ src/types/interfaces/InvoicePersistence.ts
@@ -1,6 +1,10 @@
 import { Invoice } from "../classes/Invoice";
 
 interface InvoicePersistence {
+  // * Interface having Optional Property
+  // With orthogonal persistence, a program doesn’t need to persist its state in files explicitly:
+  // the runtime handles persistence transparently.
+  isOrthogonal?: boolean;
   save(invoice: Invoice): void;
 }
 
