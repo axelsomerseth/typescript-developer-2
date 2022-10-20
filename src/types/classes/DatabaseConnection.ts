a2128a02c9a3625ede87c6b58f79898949a2ffbf
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-10-20 17:27:09 -0600

    Class extending another Class and also implementing an Interface

---
+++ src/types/classes/DatabaseConnection.ts
@@ -0,0 +1,23 @@
+class DatabaseConnection {
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  database: string;
+
+  constructor(
+    host: string,
+    port: number,
+    user: string,
+    password: string,
+    database: string
+  ) {
+    this.host = host;
+    this.port = port;
+    this.user = user;
+    this.password = password;
+    this.database = database;
+  }
+}
+
+export { DatabaseConnection };
--- src/types/classes/DatabasePersistence.ts
+++ src/types/classes/DatabasePersistence.ts
@@ -1,10 +1,23 @@
 import { InvoicePersistence } from "../interfaces/InvoicePersistence";
+import { DatabaseConnection } from "./DatabaseConnection";
 import { Invoice } from "./Invoice";
 
-// * Interface Implementation by class
-class DatabasePersistence implements InvoicePersistence {
+// * Class extending another Class and also implementing an Interface
+class DatabasePersistence
+  extends DatabaseConnection
+  implements InvoicePersistence
+{
   save(invoice: Invoice) {
     // Save to DB
+  }
+  constructor(
+    host: string,
+    port: number,
+    user: string,
+    password: string,
+    database: string
+  ) {
+    super(host, port, user, password, database);
   }
 }
 
